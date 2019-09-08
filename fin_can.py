3ca8426f8d6137b53fe530a069eb804938abc4c6
DAVID HOWE <33843929+idle-hand@users.noreply.github.com>
2019-09-08 11:50:51 -0400

    Update fin_can.py

--- fin_can.py
+++ fin_can.py
@@ -1,10 +1,7 @@
-#****************************************************************
 # caps quiz - d.e.Howe kingston ont. 2017 October
 # quiz logic when finished may offer mult. lists to quiz with
-#****************************************************************
 
 from tkinter import *
-
 import random
 import copy
 
@@ -18,16 +15,10 @@ short_list = [' ',' ',' ',' ',' ']
 score = 0
 count = 0
 
-
 new_list = copy.copy(cap)
 
-
-#****************************************************************
 # caps quiz - d.e.Howe kingston ont. 2017 October
 # function ShowChoice
-# START
-#****************************************************************
-
 
 def ShowChoice():
     global count
@@ -39,34 +30,26 @@ def ShowChoice():
           
     count = count + 1
     root.destroy()    
-#****************************************************************
-# caps quiz - d.e.Howe kingston ont. 2017 October
-# function ShowChoice
-# END
-#****************************************************************
 
+# function ShowChoice
     
 while count < 14:
 
-#****************************************************************
 # caps quiz - d.e.Howe kingston ont. 2017 October
 # shuffle my newlist a copy of the original cap[] list
 # then pull the top five from the list that are now random
 # these are now my choices for the user - since i want to insert the correct answer
 # I need to be sure it is not already in the random list i check and if it is
 # I replace it with next off shuffled list so I can then insert the correct answer at random
-#****************************************************************
+
     random.shuffle(new_list)
    
-
     for i in range(5):
         short_list[i] = new_list[i]
     
     for i in range(5):
         if short_list[i] == cap[count]:
             short_list[i] = new_list[5]
-   
-                                
     
     from random import randint
     r = randint(0, 4)
@@ -94,13 +77,6 @@ while count < 14:
     gif_one.image_create(END, image=photo)
     gif_one.grid(row=9,column=0)
    
-    
-   
-   
-   
-    
- 
-
     Label(root,text='   Score = '+ str(score),font=18).grid(row=10,column=0,sticky=W)
 
     Label(root,text=' You have: ' + str(score) + ' of ' + str(count) + ' correct.').grid(row=11,column=0)
@@ -110,7 +86,6 @@ while count < 14:
 
     done = Button(root,text =' QUIT  ',command=quit).grid(row=10,column=2) 
 
-   
     root.bind('<Return>',ShowChoice)
 
     mainloop()
